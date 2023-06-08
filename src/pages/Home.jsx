aa96d803c9b4680d69c07cb0fd8bfed82ec4bb19
Adrien <adriengoncalves2003@gmail.com>
2023-06-08 23:29:50 +0200

    add some modification

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
-import About from "../components/About";
+import { useState } from "react";
 import Header from "../components/Header";
 import Main from "../components/Main";
 import Project from "../components/Project";
 
 const Home = () => {
+  const [valueScroll, setValueScroll] = useState(0);
+
+  //const project = document.querySelector(".project");
+
+  window.addEventListener("scroll", () => {
+    setValueScroll(window.scrollY);
+    console.log(valueScroll);
+  });
+
   return (
     <div>
       <div className="home">
@@ -14,6 +23,10 @@ const Home = () => {
           <Project projectId={0} />
           <Project projectId={1} />
           <Project projectId={2} />
+
+          {valueScroll > 300 && true}
+          {valueScroll > 500 && true}
+          {valueScroll > 1000 && true}
         </div>
       </div>
     </div>
