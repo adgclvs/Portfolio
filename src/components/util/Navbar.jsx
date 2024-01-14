edcbece0ffe66dde0b8028050d6f36420b97dbc1
Adrien <adriengoncalves2003@gmail.com>
2024-01-14 22:29:46 +0100

    finish navigation ( I hope)

--- src/components/util/Navbar.jsx
+++ src/components/util/Navbar.jsx
@@ -1,31 +1,88 @@
 import React, { useEffect, useState } from "react";
 
 const Navbar = () => {
-  const [showNavbar, setShowNavbar] = useState(false);
+  const [indexSection, setIndexSection] = useState(0);
+  const [canScroll, setCanScroll] = useState(true);
+  const numberOfSections = 5;
 
   useEffect(() => {
-    setTimeout(() => {
-      setShowNavbar(true);
-    }, 250);
-  }, [showNavbar]);
+    console.log(indexSection);
+    const handleScroll = () => {
+      if (canScroll) {
+        setCanScroll(false);
+        setTimeout(() => setCanScroll(true), 500);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("wheel", handleWheel);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("wheel", handleWheel);
+    };
+  }, [canScroll]);
 
   const handleClick = (index) => {
+    setIndexSection(index);
     window.scrollTo({
       top: index * window.innerHeight,
       behavior: "smooth",
     });
   };
 
+  const handleWheel = (event) => {
+    if (!canScroll) return;
+    if (event.deltaY > 0) scrollNextSection();
+    else if (event.deltaY < 0) scrollPreviousSection();
+  };
+
+  const scrollNextSection = () => {
+    const newSection = indexSection < numberOfSections - 1 ? indexSection + 1 : indexSection;
+    setIndexSection(newSection);
+    scrollToSection(newSection);
+  };
+
+  const scrollPreviousSection = () => {
+    const newSection = indexSection > 0 ? indexSection - 1 : indexSection;
+    setIndexSection(newSection);
+    scrollToSection(newSection);
+  };
+
+  const scrollToSection = (sectionIndex) => {
+    window.scrollTo({
+      top: sectionIndex * window.innerHeight,
+      behavior: "smooth",
+    });
+  };
+
+  const renderNavigationButtons = () => {
+    return Array.from({ length: numberOfSections }, (_, index) => (
+      <button
+        key={index}
+        className={`point ${indexSection === index ? "active" : ""}`}
+        onClick={() => handleClick(index)}
+      >
+        <i className="fa-solid fa-circle fa-2xs"></i>
+      </button>
+    ));
+  };
+
   return (
-    <div className={`navbar ${showNavbar ? "show" : ""}`}>
-      <h3>Adrien.dev</h3>
-      <ul>
-        <li onClick={() => handleClick(0)}>Home</li>
-        <li onClick={() => handleClick(1)}>About</li>
-        <li onClick={() => handleClick(2)}>Projects</li>
-        <li onClick={() => handleClick(3)}>Skills</li>
-        <li onClick={() => handleClick(4)}>Contact</li>
-      </ul>
+    <div className="test">
+      <div className={`navbar`}>
+        <h3>Adrien.dev</h3>
+        <ul>
+          {["Home", "About", "Projects", "Skills", "Contact"].map((item, index) => (
+            <li key={index} onClick={() => handleClick(index)}>
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className={`nav-point`}>
+        <ul>{renderNavigationButtons()}</ul>
+      </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,75 +1,23 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import About from "../components/sections/About";
 import Contact from "../components/sections/Contact";
 import Header from "../components/sections/Header";
 import Projects from "../components/sections/Projects";
 import Skills from "../components/sections/Skills";
-import NavPoint from "../components/util/NavPoint";
 import Navbar from "../components/util/Navbar";
 
 const Home = () => {
-  const [currentHeight, setCurrentHeight] = useState(0);
-  const [canScroll, setCanScroll] = useState(true);
-  const [currentSection, setCurrentSection] = useState(0);
-
-  const handleScroll = () => {
-    if (!canScroll) {
-      return;
-    }
-
-    setCurrentHeight(window.scrollY);
-    setCanScroll(false);
-
-    setTimeout(() => {
-      setCanScroll(true);
-    }, 650);
-  };
-
-  const handleWheel = (event) => {
-    if (event.deltaY > 0 && canScroll === true) {
-      // Si la molette est descendante (scroll vers le bas)
-      scrollNextSection();
-    } else if (event.deltaY < 0 && canScroll === true) {
-      scrollPreviousSection();
-    }
-  };
-
-  const scrollNextSection = () => {
-    const sectionHeight = window.innerHeight;
-    const newCurrentSection = Math.floor(window.scrollY / sectionHeight);
-    setCurrentSection((prevSection) => (prevSection === 4 ? prevSection : prevSection + 1));
-    window.scrollTo({
-      top: sectionHeight * (newCurrentSection + 1),
-      behavior: "smooth",
-    });
-  };
-
-  const scrollPreviousSection = () => {
-    const sectionHeight = window.innerHeight;
-    const newCurrentSection = Math.floor(window.scrollY / sectionHeight);
-    setCurrentSection((prevSection) => (prevSection === 0 ? prevSection : prevSection - 1));
-    window.scrollTo({
-      top: sectionHeight * (newCurrentSection - 1),
-      behavior: "smooth",
-    });
-  };
-
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    window.addEventListener("wheel", handleWheel);
     document.body.style.overflow = "hidden";
 
     return () => {
-      window.removeEventListener("scroll", handleScroll);
-      window.removeEventListener("wheel", handleWheel);
       document.body.style.overflow = "auto";
     };
-  }, [currentHeight, canScroll]);
+  }, []);
 
   return (
     <div className="home">
       <Navbar />
-      <NavPoint sectionIndex={currentSection} />
       <Header />
       <About />
       <Projects />
